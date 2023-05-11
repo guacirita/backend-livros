3161b9f0b24a01807a914124a7e1ae7c8ceef3e7
guacirita <guacirita@gmail.com>
2023-05-10 23:27:47 -0300

    insere a nova versão do projeto integrado com o mongodb

--- livros.js
+++ livros.js
@@ -1,38 +1,97 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express"); // iniciando o express
+const router = express.Router(); // configurando a primeira parte da rota
+const cors = require('cors'); //trazendo o pacote cors que permite esta api no frontend
 
-const app = express();
-const porta = 3333;
+const conectaBancoDeDados = require("./bancoDeDados"); // ligando o arquivo com banco de dados
+conectaBancoDeDados(); // chamando a função que conecta o banco de dados
 
-const livros = [
-    {
-        nome: "Por um feminismo afro-latino-americano",
-        autora: " Lélia Gonzalez",
-        imagem: "https://m.media-amazon.com/images/P/B08GJSK6N5.01._SCLZZZZZZZ_SX500_.jpg",
-        categoria: "Política e Ciencias Sociais/"
-    },
-    {
-        nome: "Tornar-se negro: Ou As vicissitudes da identidade do negro brasileiro em ascensão social",
-        autora: " Neusa Santos Souza",
-        imagem: "https://m.media-amazon.com/images/P/B09CHGTC4C.01._SCLZZZZZZZ_SX500_.jpg",
-        categoria: "Política e Ciencias Sociais/"
-    },
-    {
-        nome: "Pele negra, máscaras brancas",
-        autora: "Frantz Fanon",
-        imagem: "https://m.media-amazon.com/images/P/B08NWFS12B.01._SCLZZZZZZZ_SX500_.jpg",
-        categoria: "Política e Ciencias Sociais/"
+const Livro = require('./LivroModel');
+
+const app = express(); // iniciando o app
+app.use(express.json());//chamando a função
+app.use(cors()); // 
+const porta = 3333; // criando a porta
+
+
+//GET
+async function mostraLivros(request, response) {
+    try {
+        const livrosVindosDoBancoDeDados = await Livro.find();
+
+        response.status(200).json(livrosVindosDoBancoDeDados);
+    }catch(erro) {
+        console.log(erro);
     }
-]
-
-function mostraLivros(request, response) {
-    response.json(livros);
+    
 }
 
+//POST
+async function criaLivro(request, response){
+    const novoLivro = new Livro({
+        nome: request.body.nome,
+        autora: request.body.autora,
+        imagem: request.body.imagem,
+        categoria: request.body.categoria
+    })
+    
+    try{
+        const livroCriado = await novoLivro.save();
+        response.status(201).json(livroCriado);
+    }catch(erro){
+        console.log(erro);
+    }
+}
+
+//PATCH
+async function corrigeLivro(request, response){
+   try{
+    const livroEncontrado = await Livro.findById(request.params.id);
+   
+    if(request.body.nome) {
+        livroEncontrado.nome = request.body.nome;
+    }
+
+    if(request.body.autora) {
+        livroEncontrado.autora = request.body.autora;
+    }
+
+    if(request.body.imagem) {
+        livroEncontrado.imagem = request.body.imagem;
+    }
+
+    if(request.body.categoria) {
+        livroEncontrado.categoria = request.body.categoria;
+    }
+
+    const livroAtualizadoNoBancoDeDados = await livroEncontrado.save();
+    response.json(livroAtualizadoNoBancoDeDados);
+    
+    }catch(erro){
+    console.log(erro);
+   }
+
+}
+
+//DELETE
+async function deletaLivro(request, response) {
+    try{
+        await Livro.findByIdAndDelete(request.params.id);
+        response.json({mensagem: "Livro deletado com sucesso!"});
+    }catch(erro){
+        console.log(erro);
+    }
+
+}
+
+//PORTA
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta", porta);
 }
 
 
-app.use(router.get('/livros', mostraLivros));
-app.listen(porta, mostraPorta);
+
+app.use(router.get('/livros', mostraLivros)); // configura rota GET/livros
+app.use(router.post('/livros', criaLivro)); // configura rota POST/livros
+app.use(router.patch('/livros/:id', corrigeLivro)); // configura a rota PATCH/livros
+app.use(router.delete('/livros/:id', deletaLivro)); // configura a rota DELETE/livros
+app.listen(porta, mostraPorta); // servidor ouvindo a porta
